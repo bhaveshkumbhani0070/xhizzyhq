0499a80c1b09547f684410fc654f8fdfc53ee1f1
kumbhani bhavesh <kumbhani.bhavesh.1@gmail.com>
2018-01-16 23:51:05 +0530

    Update for social media api

--- app/routes.js
+++ app/routes.js
@@ -1,3 +1,9 @@
-var api = require('../api/apis.js');
+var facebook = require('../api/facebook.js');
+var twitter = require('../api/twitter.js');
+var telegram = require('../api/telegram.js');
 
-module.exports = function(app) {}
+
+module.exports = function(app) {
+	    app.get('/api/facebookLikes/:pagename', facebook.facebookLikes);
+	    app.get('/api/twitter/:accountName',twitter.accountName)
+}
