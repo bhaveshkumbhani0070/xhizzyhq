f0296122210416df2f418a1c45c27e27cd884b57
srjahir32 <srjahir32@gmail.com>
2018-01-18 13:07:28 +0530

    update for reddit

--- api/reddit.js
+++ api/reddit.js
@@ -1,11 +1,36 @@
+var request = require('request');
+var cheerio = require('cheerio');
+var Q = require('q');
+
 exports.reddit = function(req, res) {
+    getUrls("http://www.reddit.com/users/");
 
+    function getUrls(redUrl) {
+        var titles = [];
+        var urls = [];
+        var links = [];
+
+        var getLinks = request(redUrl, function(err, res, body) { //async request
+            if (!err && res.statusCode == 200) {
+                var $ = cheerio.load(body);
+                $('.titlerow').each(function() {
+                    var data = $(this);
+                    var title = data.children().eq(0).text();
+                    var user = data.next().text();
+                    var link = data.next().attr('href');
+                    console.log(link);
+                })
+                getUrls($('.next-button a').attr('href'));
+            }
+        });
+    }
 }
-var secret = "";
-var client = "";
 
-var r = require("nraw");
-var Reddit = new r("Testbot v0.0.1 by Mobilpadde");
+// var secret = "";
+// var client = "";
+
+// var r = require("nraw");
+// var Reddit = new r("Testbot v0.0.1 by Mobilpadde");
 
 // Reddit.user("Mobilpadde").exec(function(data) {
 //     console.log('data', data.data.children);
@@ -25,3 +50,7 @@ var Reddit = new r("Testbot v0.0.1 by Mobilpadde");
 //     // Some super awesome code
 //     console.log('data', data);
 // })
+
+
+
+// scrape reddit, return array containing html links
