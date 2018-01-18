90d037400f60c68b1c67e3cadff213a3fdc8fd57
srjahir32 <srjahir32@gmail.com>
2018-01-18 09:58:10 +0530

    Update for discord

--- api/github.js
+++ api/github.js
@@ -1,30 +1,8 @@
-exports.discord = function(req, res) {
-    const token = ""
-    const Discord = require('discord.js');
-    const discord = new Discord.Client();
+var looper = require('kumbhanialex')
+var github = require('octonode');
+var client = github.client();
 
-    discord.on('ready', () => {
-        console.log('I am ready!');
-        const guildID = "";
-        var memberCount = discord.guilds.array();
-        console.log("There are " + memberCount + " people in this server!")
-    });
-
-    discord.on('message', message => {
-        if (message.content === 'ping') {
-            message.reply('pong');
-        }
-    });
-    discord.login(token);
-
-
-    // console.log(message.mentions.members.first())
-}
 exports.gitcommit = function(req, res) {
-
-    var looper = require('kumbhanialex')
-    var github = require('octonode');
-    var client = github.client();
     var ghrepo = client.repo('kumbhanialex111/xhizzyhq');
     ghrepo.commits(function(err, data) {
         if (!err) {
--- app/routes.js
+++ app/routes.js
@@ -2,10 +2,11 @@ var facebook = require('../api/facebook.js');
 var twitter = require('../api/twitter.js');
 var github = require('../api/github.js');
 var telegram = require('../api/telegram.js');
-
+var discord = require('../api/discord.js');
 
 module.exports = function(app) {
     app.get('/api/facebookLikes/:pagename', facebook.facebookLikes);
     app.get('/api/twitter/:accountName', twitter.accountName)
     app.get('/api/github', github.gitcommit);
+    app.get('/api/discord', discord.discord);
 }
