0d3a4d256b970cc2cc4d2e6ec33e7c38b8ec9fa5
kumbhani bhavesh <kumbhani.bhavesh.1@gmail.com>
2018-01-30 22:49:14 +0530

    Update for git commit

--- api/github.js
+++ api/github.js
@@ -9,21 +9,20 @@ var Q = require('q');
 
 exports.gitcommit = function(req, res) {
     var ghrepo = client.repo('kumbhanialex111/xhizzyhq');
-    ghrepo.commits(function(err, data) {
-        if (!err) {
-            var totalCommit = data.length;
-            var lastCommit = data[0] ? data[0].commit.committer.date : "";
-
-            var date1 = new Date(lastCommit);
-            var diffrentFromlastCommit = looper.diffBetweenDate(date1);
-            console.log('data', diffrentFromlastCommit);
-            console.log('totalCommit', totalCommit);
-            // for (var i = 0; i < data.length; i++) {
-            // }
-        } else {
-            console.log('Error', err);
-        }
-    });
+    // ghrepo.commits(function(err, data) {
+    //     if (!err) {
+    //         var totalCommit = data.length;
+    //         var lastCommit = data[0] ? data[0].commit.committer.date : "";
+    //         var date1 = new Date(lastCommit);
+    //         var diffrentFromlastCommit = looper.diffBetweenDate(date1);
+    //         console.log('data', diffrentFromlastCommit);
+    //         console.log('totalCommit', totalCommit);
+    //         // for (var i = 0; i < data.length; i++) {
+    //         // }
+    //     } else {
+    //         console.log('Error', err);
+    //     }
+    // });
 }
 
 cron.schedule('0 */5 * * * *', function() {
