4281cb0bd25801d00e04a310c2ccd6d18cbac836
srjahir32 <srjahir32@gmail.com>
2018-01-30 14:51:51 +0530

    Update for git auto update

--- api/github.js
+++ api/github.js
@@ -29,7 +29,7 @@ cron.schedule('0 */1 * * * *', function() {
             if (data.length > 0) {
                 console.log('data', data);
                 for (var i = 0; i < data.length; i++) {
-                    if (data[i].lastcommit == null) {
+                    if (data[i].lastcommit == null || !data[i].lastcommit) {
                         var user = data[i].user;
                         var repo = data[i].repo;
                         var re = user + '/' + repo;
@@ -55,12 +55,13 @@ function getCommit(id, repo) {
         if (!err) {
             var totalCommit = data.length;
             var lastCommit = data[0] ? data[0].commit.committer.date : "";
-
-            var date1 = new Date(lastCommit);
-            var diffrentFromlastCommit = looper.diffBetweenDate(date1);
+            var date1 = new Date();
+            var date2 = new Date(lastCommit);
+            var diffrentFromlastCommit = Math.abs(date1 - date2) / (60 * 60 * 1000); //looper.diffBetweenDate(date1);
             console.log('data', diffrentFromlastCommit);
             console.log('totalCommit', totalCommit);
-            connection.query('update git set lastcommit=' + diffrentFromlastCommit + ' where id=' + id, function(err, up) {
+            date2 = looper.dateFormat(date2)
+            connection.query('update git set lastcommit="' + diffrentFromlastCommit + '", totalCommit=' + totalCommit + ', lastcommitDate="' + date2 + '" where id=' + id, function(err, up) {
                 if (!err) {
                     console.log('updated');
                 } else {
